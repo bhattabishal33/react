5b356056079fc8a93e270b8b6ea8b6feff830cf0
bishal <bhattabishal330@gmail.com>
2023-04-17 21:33:58 +0545

    product

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,15 @@ app.get("/api/product", (req, res) => {
   res.send(data.products);
 });
 
+app.get("/api/product/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "product not found" });
+  }
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
--- frontend/src/screen/Productscreen.js
+++ frontend/src/screen/Productscreen.js
@@ -1,11 +1,80 @@
+import axios from "axios";
+import { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+
+import ListGroup from "react-bootstrap/ListGroup";
+import Col from "react-bootstrap/Col";
+import Rating from "../components/Rating";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, product: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function Productscreen() {
   const params = useParams();
   const { slug } = params;
-  return (
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: "",
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(`/api/product/slug/${slug}`);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+    };
+    fetchData();
+  }, [slug]);
+  return loading ? (
+    <div>loading....</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
     <div>
-      <h1>{slug}</h1>
+      <Row>
+        <Col md={6}>
+          <img
+            className="img-large"
+            src={product.image}
+            alt={product.name}
+          ></img>
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                numReview={product.numReview}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price: Rs {product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              description:
+              <p></p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}></Col>
+      </Row>
     </div>
   );
 }
